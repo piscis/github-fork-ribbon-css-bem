e7786b9619b793514b05155439733dbc7448c0d9
Alexander Pirsig <self@pirsig.net>
2015-11-17 14:23:31 +0100

    bump version

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -31,18 +31,8 @@ function inc(importance) {
   // get all the files to bump version in
   return gulp.src(['./package.json', './bower.json'])
     // bump the version number in those files
-    .pipe(filter('./package.json'))
     .pipe(bump({type: importance}))
     .pipe(gulp.dest('./'));
-    // save it back to filesystem
-    //.pipe(gulp.dest('./'))
-    // commit the changed version number
-    //.pipe(git.commit('bump version'))
-
-    // read only one file to get the version number
-    //.pipe(filter('package.json'))
-    // **tag it in the repository**
-    //.pipe(tagVersion());
 }
 
 gulp.task('patch', function() { return inc('patch'); });
